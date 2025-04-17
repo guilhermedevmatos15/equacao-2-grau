a7824198a048f2e405bc0478b95bd597a8e2f705
Guilherme Matos <devguilhermeferreira@gmail.com>
2025-04-17 11:25:02 -0300

    style: Estilizando

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,50 +14,52 @@ const Home = () => {
 	const [coeficienteC, setCoeficienteC] = useState(1);
 
 	function handleSubmit(e) {
-		console.log(e);
+		e.preventDefault();
 	}
 
 	return (
 		<div className="Home">
-			<h1>Calculadora de Equação do 2° grau</h1>
+			<main>
+				<h1>Calculadora de Equação do 2° grau</h1>
 
-			<img src={ImgEquacao} alt="Equação estrutura" />
+				<img src={ImgEquacao} alt="Equação estrutura" />
 
-			<p>Informe os coeficientes:</p>
+				<p>Informe os coeficientes:</p>
 
-			<form onSubmit={handleSubmit}>
-				<div>
-					<label>
-						<span>A</span>
-						<input
-							type="number"
-							required
-							value={coeficienteA}
-							onChange={(e) => setCoeficienteA(e.target.value)}
-						/>
-					</label>
-					<label>
-						<span>B</span>
-						<input
-							type="number"
-							value={coeficienteB}
-							onChange={(e) => setCoeficienteB(e.target.value)}
-						/>
-					</label>
-					<label>
-						<span>C</span>
-						<input
-							type="number"
-							value={coeficienteC}
-							onChange={(e) => setCoeficienteC(e.target.value)}
-						/>
-					</label>
-				</div>
+				<form onSubmit={handleSubmit}>
+					<div>
+						<label>
+							<span>A</span>
+							<input
+								type="number"
+								required
+								value={coeficienteA}
+								onChange={(e) => setCoeficienteA(e.target.value)}
+							/>
+						</label>
+						<label>
+							<span>B</span>
+							<input
+								type="number"
+								value={coeficienteB}
+								onChange={(e) => setCoeficienteB(e.target.value)}
+							/>
+						</label>
+						<label>
+							<span>C</span>
+							<input
+								type="number"
+								value={coeficienteC}
+								onChange={(e) => setCoeficienteC(e.target.value)}
+							/>
+						</label>
+					</div>
 
-				<button>Calcular</button>
-			</form>
+					<button className='btn'>Calcular</button>
+				</form>
 
-			<HistoryPreview />
+				<HistoryPreview />
+			</main>
 		</div>
 	);
 };
